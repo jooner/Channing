aa7d34e776e757bf50b1f2129b652caad58adcfe
Joon Yang <joonhyukyang@college.harvard.edu>
2018-08-20 21:06:09 -0700

    keys to share

---
+++ channing/keys.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""\
+© Copyright 2018. Channing. All rights reserved.
+
+Key Management Tool
+
+"""
+
+
+nytimes_key = 'b4fd4868eb9349c080f88aa52bc04b08'
---
+++ channing/nytimes.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""\
+© Copyright 2018. Channing. All rights reserved.
+
+NYT API Interaction Utility
+
+"""
+
+from keys import nytimes_key
